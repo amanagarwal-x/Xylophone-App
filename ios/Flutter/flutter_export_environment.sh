c7766a7b377111f1deb3b8ec7791d1962b0461b8
aman <aman.agarwal150@gmail.com>
2020-05-27 21:54:39 +0530

    cleaned build

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/ragehelix/Softwares/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/ragehelix/Documents/SY/flutter_projects/xylophone-flutter"
+export "FLUTTER_APPLICATION_PATH=/home/ragehelix/Documents/SY/flutter_projects/xylophone_app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
